72f8a280b1403b7f111b81899020fbddbf98c8b7
rupin_desai <rupin360@gmail.com>
2025-05-01 09:42:09 +0530

    Add new pages and routes for About, Contact, Projects, and Project Details

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,10 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import MainLayout from "./layouts/MainLayout";
 import HomePage from "./pages/HomePage";
+import AboutPage from "./pages/AboutPage";
+import ContactPage from "./pages/ContactPage";
+import ProjectsPage from "./pages/ProjectsPage";
+import ProjectDetailsPage from "./pages/ProjectDetailsPage";
 
 const App = () => {
   return (
@@ -9,7 +13,10 @@ const App = () => {
       <Routes>
         <Route path="/" element={<MainLayout />}>
           <Route index element={<HomePage />} />
-          {/* Add more routes here as needed */}
+          <Route path="about" element={<AboutPage />} />
+          <Route path="contact" element={<ContactPage />} />
+          <Route path="projects" element={<ProjectsPage />} />
+          <Route path="projects/:projectId" element={<ProjectDetailsPage />} />
         </Route>
       </Routes>
     </BrowserRouter>
