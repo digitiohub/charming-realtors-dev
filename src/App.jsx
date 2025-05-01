445954fe13791c5496cb33de47e2a55f72cf29d7
rupin_desai <rupin360@gmail.com>
2025-05-01 09:37:35 +0530

    Add routing and layout components; integrate framer-motion and lucide-react

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import MainLayout from "./layouts/MainLayout";
+import HomePage from "./pages/HomePage";
 
 const App = () => {
   return (
-    <div className='text-8xl'>App</div>
-  )
-}
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<MainLayout />}>
+          <Route index element={<HomePage />} />
+          {/* Add more routes here as needed */}
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
-export default App
+export default App;
